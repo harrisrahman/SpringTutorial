132b039c83b851c69f45b739dc1ab6315313fbfb
Nazumudeen, Harris <harris.nazumudeen@rbc.com>
2019-06-17 16:43:33 -0400

    Chap4 : Added registration code -> html,registration form, registration controller etc

--- src/main/java/tacos/configuration/SecurityConfig.java
+++ src/main/java/tacos/configuration/SecurityConfig.java
@@ -1,19 +1,33 @@
 package tacos.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new Pbkdf2PasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+        .passwordEncoder(encoder());
 
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("password")
-                .authorities("ROLE_USER");
     }
 }
--- src/main/java/tacos/service/UserDetailsServiceImpl.java
+++ src/main/java/tacos/service/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package tacos.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import tacos.domain.User;
