c7cd2af5b6e4a3a3e14f5784655ebf5d74773009
Nazumudeen, Harris <harris.nazumudeen@rbc.com>
2019-06-25 15:27:55 -0400

    Added security; switched to mysql database

--- src/main/java/tacos/configuration/SecurityConfig.java
+++ src/main/java/tacos/configuration/SecurityConfig.java
@@ -2,7 +2,9 @@ package tacos.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,14 +22,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder encoder(){
+    public PasswordEncoder encoder() {
         return new Pbkdf2PasswordEncoder();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
-        .passwordEncoder(encoder());
+                .passwordEncoder(encoder());
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .hasRole("USER_ROLE")
+                .antMatchers("/")
+                .access("permitAll")
+                .antMatchers("/h2-console/**")
+                .permitAll()
+                .antMatchers("/**")
+                .permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/design", true)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
     }
 }
